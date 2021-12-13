29758931ce0e1555c4fcc74d4a790e3f1fa684af
Markus <markus.oelhafen@tomorrow.one>
2021-12-13 17:53:52 +0100

    Chore: Set up iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Markus Ölhafen on 13.12.21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
