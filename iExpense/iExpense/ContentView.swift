7931b43fe2ae36a7ec44672385065dc6ddbb1af1
Markus <markus.oelhafen@tomorrow.one>
2021-12-13 20:11:17 +0100

    chore: finalize iExpense

--- iExpense/iExpense/ContentView.swift
+++ iExpense/iExpense/ContentView.swift
@@ -7,10 +7,87 @@
 
 import SwiftUI
 
+struct CurrencyText: ViewModifier {
+    let amount: Double
+    
+    var color: Color {
+        if amount < 10 {
+            return Color.red
+        } else if amount < 100 {
+            return Color.blue
+        }
+        
+        return Color.green
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .foregroundColor(color)
+    }
+}
+
 struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                Section {
+                    ForEach(expenses.items) { item in
+                        if item.type == "Personal" {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                    .modifier(CurrencyText(amount: item.amount))
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+                Section {
+                    ForEach(expenses.items) { item in
+                        if item.type == "Business" {
+                            HStack {
+                                VStack(alignment: .leading) {
+                                    Text(item.name)
+                                        .font(.headline)
+                                    Text(item.type)
+                                }
+                                
+                                Spacer()
+                                
+                                Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                    .modifier(CurrencyText(amount: item.amount))
+                            }
+                        }
+                    }
+                    .onDelete(perform: removeItems)
+                }
+            }
+            .navigationTitle("iExpense")
+            .toolbar {
+                Button {
+                    showingAddExpense = true
+                } label: {
+                    Image(systemName: "plus")
+                }
+            }
+            .sheet(isPresented: $showingAddExpense) {
+                AddView(expenses: expenses)
+            }
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
